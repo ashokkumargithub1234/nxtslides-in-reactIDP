edf2a12e7c1ff08153a2d472b1d636735ddbc081
ashokkumargithub1234 <ashokkumarpenjuvvi@gmail.com>
2023-12-09 04:39:02 +0000

    first commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Header = () => (
+  <div className="navbar">
+    <img
+      className="navLogo"
+      src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
+      alt="nxt slides logo"
+    />
+    <h1 className="nav-heading">Nxt Slides</h1>
+  </div>
+);
+export default Header;
