372edc71f7d44226273709cd9f313b6435faa4d6
ashokkumargithub1234 <ashokkumarpenjuvvi@gmail.com>
2023-12-09 04:33:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header";
+import SlidesContainer from "./Components/SlidesContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// This is the list used in the application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => (
+  <div className="app-container">
+    <Header />
+    <SlidesContainer />
+  </div>
+);
 
 export default App;
